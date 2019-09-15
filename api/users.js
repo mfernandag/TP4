a6512cb50262c025bf02fb5dc9ddaf356446a3e4
Coni <coniarganaraz@hotmail.com>
2019-09-15 18:06:29 -0300

    uniqid funcionando

---
+++ api/users.js
@@ -0,0 +1,55 @@
+const uniqid = require('uniqid');
+
+
+const users = [
+	{ id: '1', name: 'calixta', email: 'calix@ta.com' },
+	{ id: '2', name: 'calixta', email: 'calix@ta.com' },
+	{ id: '3', name: 'calixta', email: 'calix@ta.com' }
+];
+
+const getUser = (req, res, next) => {
+	res.json({ users });
+	next();
+};
+
+/*
+const patchUser = (req, res, next) => {
+	let data = req.body;
+	let index = '';
+	let resUser = users.find((e, i) => {
+		index = i;
+		return e.id === req.params.id;
+	});
+
+	if (resUser) {
+		let editedUser = { ...resUser, ...data };
+		user.splice(1, index);
+		user.push(editedUser);
+	} else {
+		res.status(404).send('no encontramos al usuario');
+	}
+};
+*/
+
+const postUser = (req, res, next) => {
+    let data = req.body;
+	if (data.hasOwnProperty('name') && data.hasOwnProperty('email')) {
+        data.id = uniqid(); 
+		users.push(data);
+		res.status('201').json(`recibido con el id ${data.id}`);
+	} else {
+		res.status('400').json('fijate que pusiste mal los datos, ameo.');
+	}
+	next();
+};
+
+const getUserByid = (req, res, next) => {
+	let resUser = users.find((e) => e.id === req.params.id);
+	if (resUser) {
+		res.json(resUser);
+	} else {
+		res.status(404).send('no encontramos al usuario');
+	}
+};
+
+module.exports = { getUser, getUserByid, postUser };
---
+++ modules/router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const users = require('../api/users');
+const router = express.Router();
+
+// PAGES ROUTES //
+router.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../pages/index.html'));
+});
+
+// API ROUTES //
+
+router.get('/api/users', users.getUser);
+router.get('/api/users/:id', users.getUserByid);
+router.post('/api/users', users.postUser);
+
+
+// NOT FOUNS HANDLER //
+router.use((req, res) => {
+	res.status(404).send('pifiaste wache');
+});
+
+module.exports = router;
